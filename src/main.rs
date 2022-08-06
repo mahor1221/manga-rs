457a40346eaeb0c9c65f5e92054c64c895145964
Mahor <mahor1221@pm.me>
2022-08-06 20:28:09 +0430

    2022-08-06_13-42-46

--- src/main.rs
+++ src/main.rs
@@ -3,78 +3,75 @@ use scraper::Html;
 use scraper::Selector;
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let manga = std::fs::read_to_string("./tmp/manga1.html")?;
     let manga = Html::parse_document(&manga);
 
     let name = Manga::name(&manga)?;
-    //let cover = Manga::cover(&manga)?;
-    //let chapters = Manga::chapters(&manga)?;
-    //let pages = Manga::pages(&chapter)?;
+    let cover = Manga::cover(&manga)?;
+    let chapters = Manga::chapters(&manga)?;
+    let pages = Manga::pages(&manga)?;
 
     dbg!(name);
-    //dbg!(cover);
+    dbg!(cover);
     //dbg!(chapters);
-    //dbg!(pages);
+    dbg!(pages);
     Ok(())
 }
 
 trait Extension {
     fn cover(manga: &Html) -> Result<Option<&str>, Box<dyn Error>>;
-    fn name(manga: &Html) -> Result<Option<&str>, Box<dyn Error>>;
+    fn name(manga: &Html) -> Result<Option<String>, Box<dyn Error>>;
     fn chapters(manga: &Html) -> Result<Option<Vec<&str>>, Box<dyn Error>>;
     fn pages(chapter: &Html) -> Result<Option<Vec<&str>>, Box<dyn Error>>;
+}
+trait Tag {
+    fn tag(manga: &Html) -> Result<Option<Vec<&str>>, Box<dyn Error>>;
 }
 
 struct Manga {}
 impl Extension for Manga {
-    fn name(manga: &Html) -> Result<Option<&str>, Box<dyn Error>> {
-        //let selector = Selector::parse(".story-info-right h1").unwrap();
-        let selector = Selector::parse("#info > h1").unwrap();
+    fn name(manga: &Html) -> Result<Option<String>, Box<dyn Error>> {
+        let selector = Selector::parse("#info h1.title span.pretty").unwrap();
         let name = manga
             .select(&selector)
-            // Returns None if String is empty
-            //.map(|e| Some(e.inner_html()).filter(|e| !e.is_empty()))
-            .map(|e| e.value().name())
-            .next();
+            .next()
+            .and_then(|e| Some(e.inner_html()));
         Ok(name)
     }
 
     fn cover(manga: &Html) -> Result<Option<&str>, Box<dyn Error>> {
-        let selector = Selector::parse(".story-info-left img").unwrap();
+        let selector = Selector::parse("#cover img").unwrap();
         let cover = manga
             .select(&selector)
-            .map(|e| e.value().attr("src"))
+            .map(|e| e.value().attr("data-src"))
             .next()
             .flatten();
         Ok(cover)
     }
 
     fn chapters(manga: &Html) -> Result<Option<Vec<&str>>, Box<dyn Error>> {
-        let selector = Selector::parse(".panel-story-chapter-list a").unwrap();
-        let chapters: Vec<&str> = manga
-            .select(&selector)
-            .filter_map(|e| e.value().attr("href"))
-            .collect();
+        let chapters = Self::name(manga);
         // Returns None if Vector is empty
         Ok(Some(chapters).filter(|v| !v.is_empty()))
     }
 
     fn pages(chapter: &Html) -> Result<Option<Vec<&str>>, Box<dyn Error>> {
         let selector =
-            Selector::parse(".container-chapter-reader img").unwrap();
-        let pages: Vec<&str> = chapter
+            Selector::parse("#thumb-container img").unwrap();
+        let pages = chapter
             .select(&selector)
             .filter_map(|e| e.value().attr("data-src"))
-            .collect();
+            .collect::<Vec<_>>();
+        // Returns None if Vector is empty
         Ok(Some(pages).filter(|v| !v.is_empty()))
     }
 }
 
-
 struct ManganeloTV {}
 impl Extension for ManganeloTV {
-    fn name(manga: &Html) -> Result<Option<&str>, Box<dyn Error>> {
+    fn name(manga: &Html) -> Result<Option<String>, Box<dyn Error>> {
         //let selector = Selector::parse(".story-info-right h1").unwrap();
         let selector = Selector::parse(".story-info-left img").unwrap();
         let name = manga
@@ -83,7 +80,8 @@ impl Extension for ManganeloTV {
             //.map(|e| Some(e.inner_html()).filter(|e| !e.is_empty()))
             .map(|e| e.value().attr("title"))
             .next()
-            .flatten();
+            .flatten()
+            .and_then(|str| Some(str.to_string()));
         Ok(name)
     }
 
