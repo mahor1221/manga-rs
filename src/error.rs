2930a9467a81bc685346c9f3358002b69a28bd06
Mahor <mahor1221@pm.me>
2022-08-14 09:20:49 +0430

    2022-08-14_04-50-33

--- src/error.rs
+++ src/error.rs
@@ -1,29 +1,33 @@
-use selectors::parser::SelectorParseErrorKind;
+pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+// if I forward scrapper erros using commented section of this mod the
+// compiler will complain on running functions with `html: &Html` variable:
+// cannot return value referencing local variable `html`
 
-type ScraperError<'a> = cssparser::ParseError<'a, SelectorParseErrorKind<'a>>;
-pub type Result<'a, T> = std::result::Result<T, Error<'a>>;
+//use selectors::parser::SelectorParseErrorKind;
+//type ScraperError<'a> = cssparser::ParseError<'a, SelectorParseErrorKind<'a>>;
+//pub type Result<'a, T> = std::result::Result<T, Error<'a>>;
 
-#[derive(Debug)]
-pub enum Error<'a> {
-    StdError(Box<dyn std::error::Error>),
-    IoError(std::io::Error),
-    ParseError(ScraperError<'a>),
-}
-
-impl<'a> From<Box<dyn std::error::Error>> for Error<'_> {
-    fn from(err: Box<dyn std::error::Error>) -> Self {
-        Error::StdError(err)
-    }
-}
-
-impl<'a> From<std::io::Error> for Error<'_> {
-    fn from(err: std::io::Error) -> Self {
-        Error::IoError(err)
-    }
-}
-
-impl<'a> From<ScraperError<'a>> for Error<'a> {
-    fn from(err: ScraperError<'a>) -> Self {
-        Error::ParseError(err)
-    }
-}
+//#[derive(Debug)]
+//pub enum Error<'a> {
+//    StdError(Box<dyn std::error::Error>),
+//    IoError(std::io::Error),
+//    ParseError(ScraperError<'a>),
+//}
+//
+//impl<'a> From<Box<dyn std::error::Error>> for Error<'_> {
+//    fn from(err: Box<dyn std::error::Error>) -> Self {
+//        Error::StdError(err)
+//    }
+//}
+//
+//impl<'a> From<std::io::Error> for Error<'_> {
+//    fn from(err: std::io::Error) -> Self {
+//        Error::IoError(err)
+//    }
+//}
+//
+//impl<'a> From<ScraperError<'a>> for Error<'a> {
+//    fn from(err: ScraperError<'a>) -> Self {
+//        Error::ParseError(err)
+//    }
+//}
--- src/main.rs
+++ src/main.rs
@@ -3,124 +3,146 @@ use scraper::Html;
 use scraper::Selector;
 mod error;
 use error::Result;
-type Url<'a> = &'a str;
+// TODO: Replace unwraps with proper errors
 
-#[tokio::main]
-async fn main() -> Result<'static, ()> {
-    let str = std::fs::read_to_string("./tmp/manga1.html")?;
-    let html = Html::parse_document(&str);
+fn main() -> Result<()> {
+    let html = std::fs::read_to_string("./tmp/manga1.html")?;
+    let comic = TestSource::comic(&html)?;
 
-    let name = TestSource::name(&html)?;
-    let cover = TestSource::cover(&html)?;
-    let pages = TestSource::pages(&html)?;
-
-    dbg!(name);
-    dbg!(cover);
-    dbg!(pages);
+    dbg!(comic);
     Ok(())
 }
 
-// TODO: check attr if returns None
+#[derive(Debug)]
+pub struct Page {
+    pub image_url: Box<str>,
+    pub thumbnail_url: Box<str>,
+}
+#[derive(Debug)]
+pub struct Chapter {
+    pub name: Option<Box<str>>,
+    pub cover_url: Option<Box<str>>,
+    pub pages: Box<[Page]>,
+}
+#[derive(Debug)]
+pub struct Comic {
+    pub name: Box<str>,
+    pub cover_url: Box<str>,
+    pub author: Box<str>,
+    pub description: Box<str>,
+    pub tags: Option<Box<[Box<str>]>>,
+    pub chapters: Box<[Chapter]>,
+}
 
-// Should be used to return None if String or &str is empty.
-// Some(String).filter(|s| !s.is_empty())
-// It's the same for Vec
-// Some(Vec<_>).filter(|v| !v.is_empty())
-
-trait Manga {
-    // Using Box<str> becouse scraper sometimes returns String sometimes &str
-    fn name(html: &Html) -> Result<Option<Box<str>>>;
-    fn cover(html: &Html) -> Result<Option<Url>>;
-    fn pages(html: &Html) -> Result<Option<Vec<Url>>>;
-}
-trait MangaChapter {
-    fn chapters(html: &Html) -> Result<Option<Vec<Url>>>;
-}
-trait MangaChapterThumbnail {
-    fn chapters_thumb(html: &Html) -> Result<Option<Vec<Url>>>;
-}
-trait MangaTag {
-    fn tags(html: &Html) -> Result<Option<Vec<Url>>>;
+trait ComicSource {
+    fn comic(html: &str) -> Result<Comic>;
 }
 
 struct TestSource {}
-impl Manga for TestSource {
-    fn name(html: &Html) -> Result<Option<Box<str>>> {
+impl ComicSource for TestSource {
+    fn comic(html: &str) -> Result<Comic> {
+        let html = Html::parse_document(&html);
+
         let selector = Selector::parse("#info h1.title span.pretty").unwrap();
-        let value = html
+        let name = html
             .select(&selector)
             .next()
-            .and_then(|e| Some(e.inner_html().into_boxed_str()))
-            .filter(|s| !s.is_empty());
-        Ok(value)
-    }
-    fn cover(html: &Html) -> Result<Option<Url>> {
-        let selector = Selector::parse("#cover img")?;
-        let value = html
+            .unwrap()
+            .inner_html()
+            .into_boxed_str();
+
+        let selector = Selector::parse("#cover img").unwrap();
+        let cover_url = html
             .select(&selector)
             .next()
-            .and_then(|e| e.value().attr("value-src"));
-        Ok(value)
-    }
-    fn pages(html: &Html) -> Result<Option<Vec<Url>>> {
-        let selector = Selector::parse("#thumbnail-container img")?;
-        let value = html
+            .unwrap()
+            .value()
+            .attr("data-src")
+            .unwrap()
+            .to_owned()
+            .into_boxed_str();
+
+        let selector = Selector::parse("#thumbnail-container img").unwrap();
+        let pages_thumbnail_url = html
             .select(&selector)
-            .filter_map(|e| e.value().attr("value-src"))
+            .map(|e| {
+                e.value()
+                    .attr("data-src")
+                    .unwrap()
+                    .to_owned()
+                    .into_boxed_str()
+            })
             .collect::<Vec<_>>();
-        let value = Some(value).filter(|v| !v.is_empty());
-        Ok(value)
-    }
-}
-impl MangaChapterThumbnail for TestSource {
-    fn chapters_thumb(html: &Html) -> Result<Option<Vec<Url>>> {
-        let selector = Selector::parse("#thumbnail-container img")?;
-        let value = html
+
+        //let selector = Selector::parse("#thumbnail-container img").unwrap();
+        let pages_image_url = html
             .select(&selector)
-            .filter_map(|e| e.value().attr("value-src"))
+            .map(|e| {
+                e.value()
+                    .attr("data-src")
+                    .unwrap()
+                    .to_owned()
+                    .into_boxed_str()
+            })
             .collect::<Vec<_>>();
-        let value = Some(value).filter(|v| !v.is_empty());
-        Ok(value)
+
+        // check two vectors lenght
+        //let pages = pages_image_url.iter().zip(pages_image_url.iter());
+
+        Ok(Comic {
+            name,
+            cover_url,
+            author: Box::from(""),
+            description: Box::from(""),
+            tags: None,
+            chapters: Box::from([Chapter {
+                name: None,
+                cover_url: None,
+                pages: Box::from([]),
+            }]),
+        })
     }
 }
 
-struct ManganeloTV {}
-impl Manga for ManganeloTV {
-    fn name(html: &Html) -> Result<Option<Box<str>>> {
-        let selector = Selector::parse(".story-info-left img")?;
-        let value = html
-            .select(&selector)
-            .next()
-            .and_then(|e| e.value().attr("title"))
-            .and_then(|s| Some(s.to_string().into_boxed_str()));
-        Ok(value)
-    }
-    fn cover(html: &Html) -> Result<Option<Url>> {
-        let selector = Selector::parse(".story-info-left img")?;
-        let value = html
-            .select(&selector)
-            .next()
-            .and_then(|e| e.value().attr("src"));
-        Ok(value)
-    }
-    fn pages(html: &Html) -> Result<Option<Vec<Url>>> {
-        let selector = Selector::parse(".container-chapter-reader img")?;
-        let value = html
-            .select(&selector)
-            .filter_map(|e| e.value().attr("value-src"))
-            .collect::<Vec<_>>();
-        let value = Some(value).filter(|v| !v.is_empty());
-        Ok(value)
-    }
-}
-impl MangaChapter for ManganeloTV {
-    fn chapters(html: &Html) -> Result<Option<Vec<Url>>> {
-        let selector = Selector::parse(".panel-story-chapter-list a")?;
-        let value = html
-            .select(&selector)
-            .filter_map(|e| e.value().attr("href"))
-            .collect::<Vec<_>>();
-        let value = Some(value).filter(|v| !v.is_empty());
-        Ok(value)
-    }
-}
+//struct ManganeloTV {}
+//impl Manga for ManganeloTV {
+//    fn name(html: &Html) -> Result<Box<str>> {
+//        let selector = Selector::parse(".story-info-left img")?;
+//        let value = html
+//            .select(&selector)
+//            .next()
+//            .and_then(|e| e.value().attr("title"))
+//            .and_then(|s| Some(s.to_string().into_boxed_str()))
+//            .filter(|s| !s.is_empty());
+//        Ok(value)
+//    }
+//    fn cover(html: &Html) -> Result<Url> {
+//        let selector = Selector::parse(".story-info-left img")?;
+//        let value = html
+//            .select(&selector)
+//            .next()
+//            .and_then(|e| e.value().attr("src"))
+//            .filter(|s| !s.is_empty());
+//        Ok(value)
+//    }
+//    fn pages(html: &Html) -> Result<Vec<Url>> {
+//        let selector = Selector::parse(".container-chapter-reader img")?;
+//        let value = html
+//            .select(&selector)
+//            .filter_map(|e| e.value().attr("data-src").filter(|s| !s.is_empty()))
+//            .collect::<Vec<_>>();
+//        let value = Some(value).filter(|v| !v.is_empty());
+//        Ok(value)
+//    }
+//}
+//impl MangaChapter for ManganeloTV {
+//    fn chapters(html: &Html) -> Result<Vec<Url>> {
+//        let selector = Selector::parse(".panel-story-chapter-list a")?;
+//        let value = html
+//            .select(&selector)
+//            .filter_map(|e| e.value().attr("href").filter(|s| !s.is_empty()))
+//            .collect::<Vec<_>>();
+//        let value = Some(value).filter(|v| !v.is_empty());
+//        Ok(value)
+//    }
+//}
